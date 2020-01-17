e909740be9770ade15de65afec043bb9205e8945
James Sevedge <j.sevedge@f5.com>
2020-01-17 15:22:21 -0800

    create dir

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -6,6 +6,9 @@ NAME=$(cat ${MAINDIR}/package.json | jq .name -r)
 # set permissions
 chmod -R 744 scripts/*
 
+# create artifact directory
+mkdir -p dist
+
 # tar and zip
 tar -C ${MAINDIR} --exclude=${PWD##*/}/dist -cf dist/${NAME}.tar src
 gzip -nf dist/${NAME}.tar
