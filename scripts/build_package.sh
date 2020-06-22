3c265ac67dfe310a76b1b12081f6bec50f727624
Andrey Kashcheev <a.kashcheev@f5.com>
2020-06-22 14:01:11 -0700

    ESECLDTPLT-1945: RPM package generation

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -4,58 +4,79 @@ MAINDIR="$(dirname $0)/.."
 NAME=$(cat ${MAINDIR}/package.json | jq .name -r)
 CLOUDS=(azure aws gcp all)
 COMMON_DEPENDENCIES=$(cat ${MAINDIR}/package.json | jq -r ".dependencyMap.common")
+VERSION=$(cat ${MAINDIR}/package.json | jq -r ".version")
+RELEASE=$(cat ${MAINDIR}/package.json | jq -r ".release")
 
-# delete dist if it exists
-echo "Cleaning dist/ directory"
+echo "*** Clean up dist/ directory"
 rm -r dist/
-echo "Generating JS code using tsc command"
-# generating JS from TypeScript
+echo "*** Generate JS code using tsc command"
 tsc
-echo "Switching to dist to install node_modules"
-# installing node_modules
-cd dist/
-echo "Installing node_modules"
-npm install
-# switching back to main
-echo "Switching back to main directory"
-cd ..
-# set permissions
+
+echo "*** Set persmissions on scripts"
 chmod -R 744 ${MAINDIR}/scripts/*
 
+echo "*** Create dist/rpms directory"
+mkdir -p dist/rpms
 for cloud in "${CLOUDS[@]}"; do
-    # create artifact directory
-    mkdir -p dist/${cloud}
+    echo "*** Create ${cloud} artifact directory"
+    mkdir -p dist/${cloud}/rpmbuild
+    mkdir -p dist/working/${cloud}
 
-    # build cloud-specific package.json
+    echo "*** Build cloud-specific package.json"
     if [[ ${cloud} == "all" ]]; then
-        cp ${MAINDIR}/dist/package.json ${MAINDIR}/environments/${cloud}
+        cp ${MAINDIR}/dist/package.json ${MAINDIR}/dist/working/${cloud}
     else
         cloud_dependencies=$(cat ${MAINDIR}/dist/package.json | jq -r ".dependencyMap.${cloud}")
         final_dependencies=$(jq --argjson common "${COMMON_DEPENDENCIES}" --argjson cloud "${cloud_dependencies}" -n '$common + $cloud')
-        jq --argjson final "${final_dependencies}" '.dependencies = $final' ${MAINDIR}/dist/package.json > ${MAINDIR}/environments/${cloud}/package.json
+        jq --argjson final "${final_dependencies}" '.dependencies = $final' ${MAINDIR}/dist/package.json > ${MAINDIR}/dist/working/${cloud}/package.json
     fi
 
-    # install cloud-specific dependencies
-    npm install --prefix ${MAINDIR}/environments/${cloud} --production
+    echo "*** Install cloud-specific dependencies"
+    npm install --prefix ${MAINDIR}/dist/working/${cloud} --production
 
-    # copy src
-    cp -r ${MAINDIR}/dist/src ${MAINDIR}/environments/${cloud}
+    echo "*** Copy source code to working"
+    cp -r ${MAINDIR}/dist/src ${MAINDIR}/dist/working/${cloud}
 
-    # tar and zip
-    tar -C ${MAINDIR}/environments/${cloud} --exclude=${PWD##*/}/dist -cf dist/${cloud}/${NAME}-${cloud}.tar src node_modules package.json
-    gzip -nf dist/${cloud}/${NAME}-${cloud}.tar
+    echo "*** Generate RPM Package"
+    rpmbuild --quiet -bb \
+       --define "main $(pwd)" \
+       --define "_topdir %{main}/dist/${cloud}/rpmbuild" \
+       --define "_name ${NAME}-${cloud}" \
+       --define "_version ${VERSION}" \
+       --define "_release ${RELEASE}" \
+       --define "cloud ${cloud}" \
+       f5-bigip-runtime-init.spec
+    OUTPUT=$(ls -t dist/${cloud}/rpmbuild/RPMS/noarch/*.rpm 2>/dev/null | head -1)
+    cp ${OUTPUT} ${MAINDIR}/dist/${cloud}
+    echo "*** Built RPM: ${OUTPUT##*/}"
 
-    # create sha256 hash
-    dir=$(pwd)
-    cd dist/${cloud}/
-    openssl dgst -sha256 ${NAME}-${cloud}.tar.gz > ${NAME}-${cloud}.tar.gz.sha256
-    cd $dir
+    echo "*** Sign package titled as ${NAME}-${cloud}-${VERSION}-${RELEASE}.noarch.rpm"
+    curl -sk -X POST -H "Authorization: Bearer $CM_SIGNER_ACCESS_TOKEN" -F "file=@dist/${cloud}/${NAME}-${cloud}-${VERSION}-${RELEASE}.noarch.rpm" -o "dist/rpms/${NAME}-${cloud}-${VERSION}-${RELEASE}-signed.noarch.rpm" "https://cm-signer.pdsea.f5net.com/api/v1/sign/rpm"
+    echo "*** Set execution permission ${NAME}-${cloud}-${VERSION}-${RELEASE}-signed.noarch.rpm"
+    chmod +x dist/rpms/${NAME}-${cloud}-${VERSION}-${RELEASE}-signed.noarch.rpm
+    echo "*** Completed signing packge. Deleting original package: ${NAME}-${cloud}-${VERSION}-${RELEASE}.noarch.rpm"
+    rm dist/${cloud}/${NAME}-${cloud}-${VERSION}-${RELEASE}.noarch.rpm
+    echo "*** Create SHA-256 file"
+    cd dist/rpms/
+    sha256sum ${NAME}-${cloud}-${VERSION}-${RELEASE}-signed.noarch.rpm > ${NAME}-${cloud}-${VERSION}-${RELEASE}-signed.noarch.rpm.sha256
+    cd ../..
+    echo "*** Created SHA-256 checksum files"
 
-    # clean up
-    rm ${MAINDIR}/environments/${cloud}/package.json
-    rm -r ${MAINDIR}/environments/${cloud}/node_modules
-    rm -r ${MAINDIR}/environments/${cloud}/src
+    echo "*** Cleanup for ${cloud} cloud ..."
+    rm -r dist/${cloud}
+    rm -r dist/working/${cloud}
 done
 
-# create sha256 of install.sh
-openssl dgst -sha256 scripts/install.sh > dist/install.sh.sha256
+echo "*** Remove working files from dist/"
+rm -r dist/working
+rm -r dist/src
+rm -r dist/package.json
+
+echo "*** Start creating makeself package"
+echo "*** Copy over install script"
+cp ${MAINDIR}/scripts/install_rpm.sh ${MAINDIR}/dist/rpms
+echo "*** Set permissions on install script to allow exectuon"
+chmod +x ${MAINDIR}/dist/rpms/install_rpm.sh
+echo "*** Generate makeself package"
+${MAINDIR}/scripts/makeself.sh ${MAINDIR}/dist/rpms dist/${NAME}-${VERSION}-${RELEASE}.gz.run "F5 BIGIP Runtime Init installation" ./install_rpm.sh
+
