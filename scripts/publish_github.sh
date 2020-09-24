d279825dd97bdd0a75c020de858d0f78049169e9
Michael Shimkus <m.shimkus@f5.com>
2020-09-24 13:21:05 -0700

    Document JSON schema and configuration examples

--- scripts/publish_github.sh
+++ scripts/publish_github.sh
@@ -16,7 +16,7 @@ git config user.email $GITLAB_USER_EMAIL
 RELEASE_VERSION=$(echo $CI_COMMIT_REF_NAME | awk -F"-" '{ print $2 }')
 RELEASE_BUILD=$(echo $CI_COMMIT_REF_NAME | awk -F"-" '{ print $3 }')
 ALLOWED_DIRS=(src examples diagrams)
-ALLOWED_FILES=(.gitignore .gitallowed package.json package-lock.json README.md tsconfig.json LICENSE)
+ALLOWED_FILES=(.gitignore .gitallowed package.json package-lock.json README.md SCHEMA.md tsconfig.json LICENSE)
 
 echo "*** Setting git origin"
 git remote rm origin && git remote add origin git@github.com:f5devcentral/f5-bigip-runtime-init.git
@@ -34,10 +34,6 @@ for file in "${ALLOWED_FILES[@]}"; do
     git checkout HEAD ${file}
     git add ${file}
 done
-
-echo "*** Rendering README.md"
-node ./scripts/README_render.js
-cat ./README.md
 
 echo "*** Committing source code"
 git status
